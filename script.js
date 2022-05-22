e6b7430c57a8a770b2d22cc6c58cd3503bba7644
ravencancio <102492480+arcan9@users.noreply.github.com>
2022-05-22 03:18:23 -0700

    Add axios and get author name from API
    test functionality

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-// Create a function to store the API url and key
+// Create an event listener for when a user clicks a certain section
+// it makes a call to a function that...
 
-// Create a function that takes the API
+// Takes an API with a certain parameter
+// For example, "insightful," and axios fetches this data.
+// Then it calls a...
+
+// Function that takes that result and pulls a random book title
+// recommends it to the user
+// display book cover, title, author, and summary(?)
+
+function clickMood() {
+  let apiKey = "AIzaSyAKH5VZTVN3qjnDH0PKgJU6TcPgOX-uNYg";
+  let apiUrl = `https://www.googleapis.com/books/v1/volumes?q=life+happiness+positive+insubject:self-help&filter=ebooks&key=${apiKey}`;
+
+  axios.get(apiUrl).then(myFunc);
+}
+
+function myFunc(response) {
+  console.log(response);
+  console.log(response.data.items[0].volumeInfo.authors);
+}
+
+let myMood = document.querySelector(".mood");
+myMood.addEventListener("click", clickMood);
