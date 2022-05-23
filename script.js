5f06747aaf29c388fc1d4823fdf7006a95a014ec
ravencancio <102492480+arcan9@users.noreply.github.com>
2022-05-23 04:01:11 -0700

    Replace DOM element with book title
    book title obtained from object array

--- script.js
+++ script.js
@@ -7,19 +7,23 @@
 
 // Function that takes that result and pulls a random book title
 // recommends it to the user
-// display book cover, title, author, and summary(?)
+// display book cover, title, author, and summary(?) by DOM manipulation
 
 function clickMood() {
-  let apiKey = "AIzaSyAKH5VZTVN3qjnDH0PKgJU6TcPgOX-uNYg";
+  let apiKey = "AIzaSyAKH5VZTVN3qjnDH0PKgJU6TcPgOX-uNYg"; // hide this key later
   let apiUrl = `https://www.googleapis.com/books/v1/volumes?q=life+happiness+positive+insubject:self-help&filter=ebooks&key=${apiKey}`;
 
-  axios.get(apiUrl).then(myFunc);
+  axios.get(apiUrl).then(recommendTitle);
 }
 
-function myFunc(response) {
+function recommendTitle(response) {
   console.log(response);
   let randomArray = Math.floor(Math.random() * response.data.items.length);
-  console.log(response.data.items[randomArray].volumeInfo.title);
+  let bookRec = response.data.items[randomArray].volumeInfo.title;
+  let bookTitleDiv = document.querySelector(".bookTitle");
+
+  // Manipulate DOM
+  bookTitleDiv.textContent = `${bookRec}`;
 }
 
 let myMood = document.querySelector(".mood");
